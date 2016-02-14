3e269c4568f4974bea2bb11605b21d08cdc875b1
Ahmed Aziz <ahmed83@me.com>
2016-02-14 14:41:16 +0100

    Adding starting and destination input on the header

--- map.js
+++ map.js
@@ -4,9 +4,8 @@ if (Meteor.isClient) {
 
 
   Template.amsterdamMap.helpers({
-
-      'showMap' : function(){
-          return "Ahmed Aziz. Amsterdam 2016"
+      'text': function () {
+          return "text van template helper"
     }
   });
 
@@ -22,12 +21,39 @@ if (Meteor.isClient) {
         }).setView([52.37783, 4.89995], 13);
 
         L.tileLayer.provider('MapQuestOpen.OSM').addTo(showMap);
+
+        L.map('showMap', {
+            searchControl: {layer: searchLayer}
+        });
+
+
+        //L.Routing.control({
+        //    waypoints: [
+        //        L.latLng(57.74, 11.94),
+        //        L.latLng(57.6792, 11.949)
+        //    ],
+        //    routeWhileDragging: true
+        //}).addTo(showMap);
+
         showMap.spin(false);
     };
+
+    Template.addDestination.events({
+        'submit form': function () {
+            event.preventDefault();
+
+            var start = event.target.start.value;
+            console.log(start);
+
+            var end = event.target.end.value;
+            console.log(end);
+        }
+    });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
   });
 }
